2b50ba47ebd705b97b4e7dfe2ce9fd0f0faaa927
ololadetemi <ololadetoluwalase@gmail.com>
2024-08-28 16:34:35 +0100

    made some changes

--- script.js
+++ script.js
@@ -5,3 +5,19 @@ menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x')
     navbar.classList.toggle('active');
 }
+
+function toggleReadMore() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("readMoreBtn");
+
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.textContent = "Read More";
+        moreText.style.display = "none";
+    } else {
+        dots.style.display = "none";
+        btnText.textContent = "Read Less";
+        moreText.style.display = "inline";
+    }
+}
